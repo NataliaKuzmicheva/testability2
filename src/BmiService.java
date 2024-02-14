9d3c9baf75dbe554cb3ca183f48e20b900847953
Natalia Kuzmicheva <kuzmichyova.natalie@yandex.ru>
2024-02-14 10:33:34 +0300

    Change type of variable

--- src/BmiService.java
+++ src/BmiService.java
@@ -2,8 +2,8 @@ public class BmiService {
 
     //ИМТ = масса / рост^2//
 
-    public double calculate(int weightKilo, double heightMetres) {
-        double bmi = weightKilo / (heightMetres * heightMetres);
+    public int calculate(int weightKilo, double heightMetres) {
+        int bmi = (int) (weightKilo / (heightMetres * heightMetres));
         return bmi;
 
     }
